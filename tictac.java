9706dc37ac44a62da8b1232047a225731f3b80f9
Theo <tad3@illinois.edu>
2017-12-11 18:12:29 -0600

    Update tictac.java

--- tictac.java
+++ tictac.java
@@ -1,3 +1,22 @@
 //
-//This will be the main program to run the actual game in the terminal.
+//This will be the main program for running the game in the terminal.
 //
+public class tictac	{
+	
+	private static void main(final String[] args)	{
+		
+		//initialize and assign x and o to players
+		
+		//initialize board
+		
+		//accept inputs and print the current board, check if win or tie each turn
+		
+		//if win or tie, end the game and declare the winner, or if there was a tie.
+		
+		//prompt to restart program.
+		
+		
+		
+	}
+	
+}
